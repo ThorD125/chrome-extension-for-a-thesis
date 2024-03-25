97556146ecd1a553947e3efcc447abef02048965
ther <thor.demeestere@telenet.be>
2024-03-25 21:18:22 +0100

    fix popup

--- popup.js
+++ popup.js
@@ -1,11 +1,15 @@
 import { readFromDatabase } from "./utils/database.js";
 import { populizePopup } from "./utils/helpers.js";
 
+import { getCurrentTabId } from "./utils/tabmanager.js";
+
 console.log("popup.js");
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOMContentLoaded");
-  readFromDatabase("attackingDatabase", "tabid")
-    .then((data) => populizePopup(data))
-    .catch((error) => console.error(error));
+  getCurrentTabId().then((tabId) => {
+    readFromDatabase("attackingDatabase", tabId)
+      .then((data) => populizePopup(data))
+      .catch((error) => console.error(error));
+  });
 });
