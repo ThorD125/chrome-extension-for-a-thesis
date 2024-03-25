cb29974131443d2245ead41bb4d39abfe7c6bb92
thor <thor.demeestere@telenet.be>
2024-03-25 14:27:28 +0100

    imports in responses

--- setup/gatheringResponse.js
+++ setup/gatheringResponse.js
@@ -1,3 +1,5 @@
+import { appendToObjectList } from "../utils/database.js";
+
 export default async function setupGatheringResponse() {
   const onMessage = (result) => {
     const theresults = result.results.filter((x) => {
@@ -33,52 +35,4 @@ export default async function setupGatheringResponse() {
     }
   };
   chrome.runtime.onMessage.addListener(onMessage);
-}
-
-function openDatabase() {
-  return new Promise((resolve, reject) => {
-    const request = indexedDB.open("myDatabase", 1);
-
-    request.onupgradeneeded = function (event) {
-      const db = event.target.result;
-      if (!db.objectStoreNames.contains("attackingDatabase")) {
-        db.createObjectStore("attackingDatabase", { keyPath: "id" });
-      }
-    };
-
-    request.onerror = function (event) {
-      reject("Database error: " + event.target.errorCode);
-    };
-
-    request.onsuccess = function (event) {
-      resolve(event.target.result);
-    };
-  });
-}
-
-async function appendToObjectList(identifier, newObj) {
-  const db = await openDatabase();
-  const transaction = db.transaction(["attackingDatabase"], "readwrite");
-  const store = transaction.objectStore("attackingDatabase");
-  return new Promise((resolve, reject) => {
-    const request = store.get(identifier.id);
-    request.onsuccess = () => {
-      let data = request.result;
-      if (data) {
-        // If the entry exists, append the new object to the list
-        data.attacks.push(newObj);
-      } else {
-        // If the entry does not exist, create it with newObj as the first item in the list
-        data = {
-          id: identifier.id,
-          title: identifier.title,
-          attacks: [newObj],
-        };
-      }
-      const updateRequest = store.put(data);
-      updateRequest.onsuccess = () => resolve("Object appended successfully.");
-      updateRequest.onerror = () => reject("Error appending object.");
-    };
-    request.onerror = () => reject("Error fetching existing data.");
-  });
 }
--- utils/database.js
+++ utils/database.js
@@ -29,3 +29,29 @@ export async function readFromDatabase(key) {
     request.onerror = () => reject("Error fetching data.");
   });
 }
+export async function appendToObjectList(identifier, newObj) {
+  const db = await openDatabase();
+  const transaction = db.transaction(["attackingDatabase"], "readwrite");
+  const store = transaction.objectStore("attackingDatabase");
+  return new Promise((resolve, reject) => {
+    const request = store.get(identifier.id);
+    request.onsuccess = () => {
+      let data = request.result;
+      if (data) {
+        // If the entry exists, append the new object to the list
+        data.attacks.push(newObj);
+      } else {
+        // If the entry does not exist, create it with newObj as the first item in the list
+        data = {
+          id: identifier.id,
+          title: identifier.title,
+          attacks: [newObj],
+        };
+      }
+      const updateRequest = store.put(data);
+      updateRequest.onsuccess = () => resolve("Object appended successfully.");
+      updateRequest.onerror = () => reject("Error appending object.");
+    };
+    request.onerror = () => reject("Error fetching existing data.");
+  });
+}
